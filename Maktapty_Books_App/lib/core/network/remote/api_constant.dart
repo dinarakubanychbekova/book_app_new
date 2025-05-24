f2246530d44240351ad1cd67c8341d465e57d9bd
dinara <dinarakubanychbekoba71@gmail.com>
2025-05-25 02:28:02 +0600

    Initial commit with latest version

---
+++ Maktapty_Books_App/lib/core/network/remote/api_constant.dart
@@ -0,0 +1,11 @@
+class ApiConstant {
+  static const String BASE_URL =
+      "https://api.nytimes.com/svc/books/v3/lists/current/";
+  static const String API_KEY = "AW2W9T2v18ccDGzd7Z4iShihGfxAiR02";
+  static const String HEDER_COVER_FICTION =
+      'hardcover-fiction.json?api-key=$API_KEY';
+  static const String MANGA = 'manga.json?api-key=$API_KEY';
+  static const String PICKER_BOOK = 'health.json?api-key=$API_KEY';
+  static const String SCIENCE = 'e-book-fiction.json?api-key=$API_KEY';
+  static const String SPORT = 'audio-fiction.json?api-key=$API_KEY';
+}
